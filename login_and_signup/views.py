11cd368503625d4da8cb720e9803f028784c6d25
Jacky Huang <jacky_huang56@hotmail.com>
2021-09-11 16:29:01 -0600

    wrote some code for when we stitch up signup and profiles together

--- login_and_signup/views.py
+++ login_and_signup/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#TODO
+# Add this code to when a user signs up
+
+# from profile_page.models import User
+# user = User(username = request.user.username, first_name =request.user.firstname, last_name = request.user.lastname, weight = 0, age = 0, height = 0)
+# user.save()
