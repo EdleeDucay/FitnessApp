5726c46f8f7f64e9ae88888524d2725cf3658f53
Jacky Huang <jacky_huang56@hotmail.com>
2021-09-12 09:34:28 -0600

    changed default firstname and last name when signing up

--- login_and_signup/views.py
+++ login_and_signup/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 # Add this code to when a user signs up
 
 # from profile_page.models import User
-# user = User(username = request.user.username, first_name =request.user.firstname, last_name = request.user.lastname, weight = 0, age = 0, height = 0)
+# user = User(username = request.user.username, first_name = "John", last_name = "Doe", weight = 0, age = 0, height = 0)
 # user.save()
