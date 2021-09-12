b5dfee0ddad650ea672fe76e6b020f6306fbff45
techc <bcen@ualberta.ca>
2021-09-11 18:31:56 -0600

    CSS for login page.

--- accounts/users/views.py
+++ accounts/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 # Create your views here.
 
 def home(request):
